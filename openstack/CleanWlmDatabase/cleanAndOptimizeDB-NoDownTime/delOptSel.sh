270926b52b2b0eacfab1e62bab65104621addd42
Rajneesh <rajneesh.kapoor@trilio.io>
2022-04-11 10:50:01 +0000

    Check in scripts & other artifacts to enable DB clean up without downtime

--- openstack/CleanWlmDatabase/cleanAndOptimizeDB-NoDownTime/delOptSel.sh
+++ openstack/CleanWlmDatabase/cleanAndOptimizeDB-NoDownTime/delOptSel.sh
@@ -21,11 +21,12 @@ function logMsg() {
 	echo -e "`date +"%m/%d/%y %H:%M:%S"` : $1" 1>>LOGS/${tableName}.log 2>&1
 }
 
-grep -v '^#' ${fileName} | while read line
+grep -v -e '^#' -e '^$' ${fileName} | while read line
 do
 	echo $var | awk -F"FROM" '{print $2}' | cut -d" " -f2
 	tableName=$(echo $line | awk -F"FROM" '{print $2}' | cut -d" " -f2)
 	logMsg "Running Query $(echo $line | sed -e "s/#LIMIT#/${batchSize}/g")"
+	totalDelRecs=0
 	#for i in `seq 1 ${iterations}`
 	for (( ; ; )) #Infinite loop to break only when ${batchSize} < actual number of records deleted
 	do
@@ -34,15 +35,17 @@ do
 		mOut=$(mysql -e "$(echo $line | sed "s/#LIMIT#/${batchSize}/")" ${database} -vv)
 		if [ $? != 0 ]
 		then
-			logMsg "Command [ ${line} ] failing; Exiting !!"
+			logMsg "\tCommand [ ${line} ] failing; Exiting !!"
 			exit 1
 		fi
 		recCount=$(echo ${mOut} | awk '{print $(NF-3)}')
-		logMsg "${recCount} Records deleted"
+		logMsg "\tCurrently, ${recCount} Records deleted from ${tableName}"
+		totalDelRecs=$( expr ${totalDelRecs} + ${recCount} )
 		#if [ ${recCount} -le ${batchSize} ]
 		if [ ${recCount} -eq 0 ]
 		then
 			logMsg "DELETE DONE FOR ${tableName}; EXITING DELETE LOOP !!"
+			logMsg "Total ${totalDelRecs} records deleted from ${tableName}"
 			break
 		fi
 		logMsg "\tWaiting for ${waitInSec} sec"
